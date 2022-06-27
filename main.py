d315e5c9cfa74497a63292a0d42dba48674633df
Akash Chaurasia <akashchaurasiya25@gmail.com>
2022-06-27 18:46:50 +0530

    File Upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import cv2
+from random import randrange as r
+#dataset load
+trainedData = cv2.CascadeClassifier('Face.xml')
+#start cam
+cam = cv2.VideoCapture('20211010_223036.mp4')
+while True:
+    success, frame = cam.read()
+    grayimg = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    #detect faces
+    faceCoordinates = trainedData.detectMultiScale(grayimg)
+    for x,y,w,h in faceCoordinates:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (r(0, 256), r(0, 256), r(0, 256)), 2)
+
+    # display image
+    cv2.imshow('Result', frame)
+    key = cv2.waitKey(1)
+    if (key == 81 or key == 113):
+        break
+
+print("End of Program")
+
+
